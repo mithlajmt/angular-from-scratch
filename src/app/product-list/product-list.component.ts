064608c2461890e3763d5ad145df08aa12152b8f
mithlajmatta <144442116+mithlajmatta@users.noreply.github.com>
2024-01-08 00:02:40 +0530

    propert binding

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -11,7 +11,8 @@ export class ProductListComponent {
     price : 789,
     color : "red",
     discount: 8.5,
-    inStock:22
+    inStock:22,
+    pImage:'https://www.cnet.com/a/img/resize/aa6701977dfc0cc39bb89d3885105b84658ac3aa/hub/2023/10/04/449da917-ee79-46a2-8fba-928978f4b096/iphone-15-29.jpg?auto=webp&fit=crop&height=675&width=1200'
   }
 
   getDiscountedPrice(){
