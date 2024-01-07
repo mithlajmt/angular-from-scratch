af40ed65bd5b65cc3ab836982978d5184d7a52c8
mithlajmatta <144442116+mithlajmatta@users.noreply.github.com>
2024-01-07 21:31:51 +0530

    ekart first commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,9 +3,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:'app-header',
-    template:'<h1>hey its on</h1>'
-
-
+    templateUrl:'./header.component.html',
+    styleUrls:['./header.component.css']
 })
 
 export class HeaderComponent {
